8a9a58f0d5a313e4fd72ffb010958ae957bf8831
Sylvain Kamdem <>
2021-06-01 16:59:36 +0000

    Updated hello.js to print 'hello world!

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+  //console.log("Hello world!");
+	return "Hello world!";
 }
  
 module.exports = hello;
